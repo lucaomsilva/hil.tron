5928dbf5997b5de142c7a527b6f9ae46dcf1d2e8
Lucas Moura da Silva <lucas.silva16@unifesp.br>
2026-08-25 15:30:02 -0300

    feat: add receiver module and format the hardware description

--- rtl/uart/uart.v
+++ rtl/uart/uart.v
@@ -1,39 +1,74 @@
 module uart #(
-    parameter integer CLOCK = 27000000,  // Tang Nano 9K - 27MHz
-    parameter integer BAUDRATE = 115200, // buadrate required
-    parameter integer BAUDCLOCK = 1,     // number of tick in one BCLK
+    parameter integer CLOCK     = 27000000,  // Tang Nano 9K - 27MHz
+    parameter integer BAUDRATE  = 115200,    // buadrate required
+    parameter integer BAUDCLOCK = 16,        // 16x oversampled clock for receiver
 
     parameter integer DATA_WIDTH = 8,
     parameter integer FIFO_DEPTH = 16
 ) (
     input wire clk,
     input wire rst,
-    input wire tx_en,
 
-    output wire tx
+    input wire                  tx_en,
+    input wire [DATA_WIDTH-1:0] tx_in,
+
+    input wire rx,
+
+    output wire tx,
+
+    output wire                  rx_data_ready,
+    output wire [DATA_WIDTH-1:0] rx_out
 );
 
-  // ---- Baudrate ----
-  wire bclk;
-
+  // ---- Baudrate (16x for Receiver) ----
+  wire bclk_16x;
   baudrate #(
       .CLOCK(CLOCK),
       .BAUDRATE(BAUDRATE),
-      .BAUDCLOCK(BAUDCLOCK)
-  ) baudrate_inst (
+      .BAUDCLOCK(16)
+  ) baudrate_rx_inst (
       .clk (clk),
       .rst (rst),
-      .bclk(bclk)
+      .bclk(bclk_16x)
+  );
+
+  // ---- Baudrate (1x for Transmitter) ----
+  wire bclk_1x;
+  baudrate #(
+      .CLOCK(CLOCK),
+      .BAUDRATE(BAUDRATE),
+      .BAUDCLOCK(1)
+  ) baudrate_tx_inst (
+      .clk (clk),
+      .rst (rst),
+      .bclk(bclk_1x)
   );
 
   // ---- Transmitter ----
-  transmitter transmitter_inst (
+  transmitter #(
+      .DATA_WIDTH(DATA_WIDTH),
+      .PTR_WIDTH ($clog2(FIFO_DEPTH))
+  ) transmitter_inst (
       .clk(clk),
-      .bclk(bclk),
+      .bclk(bclk_1x),
       .rst(rst),
       .tx_en(tx_en),
-      .tx_in(8'h41),  // Static 'A'
+      .tx_in(tx_in),
       .tx(tx)
+  );
+
+  // ---- Receiver ----
+  receiver #(
+      .DATA_WIDTH(DATA_WIDTH),
+      .PTR_WIDTH ($clog2(FIFO_DEPTH))
+  ) receiver_inst (
+      .clk(clk),
+      .rst(rst),
+      .bclk(bclk_16x),
+      .rx(rx),
+      .rx_en(rx_data_ready),
+      .rx_out(rx_out),
+      .data_ready(rx_data_ready)
   );
 
 endmodule
