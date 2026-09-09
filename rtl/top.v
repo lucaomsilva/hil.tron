1e3cb2cfb1df4129b7f95f9b3086148b975c3d53
Lucas Moura da Silva <lucas.silva16@unifesp.br>
2026-09-08 23:40:20 -0300

    feat: add reference and controller module in top

--- rtl/top.v
+++ rtl/top.v
@@ -21,7 +21,7 @@ module top (
 
   uart uart_inst (
       .clk(CLK),
-      .rst(RST),
+      .rst(1'b1),
       .tx_en(tx_en),
       .tx_in(tx_data),
       .tx_ready(tx_ready),
@@ -33,32 +33,81 @@ module top (
   );
 
   // --- Decode & Encode Loopback ---
-  wire [31:0] loop_data;
-  wire        loop_ready;
-  wire        loop_idle;
+  wire [31:0] decode_data_out;
+  wire        decode_ready;
 
   // --- Decode ---
   decode decode_inst (
       .clk(CLK),
-      .rst(RST),
+      .rst(1'b1),
       .data_ready(rx_ready),
       .data_encode(rx_data),
       .decode_en(rx_en),
-      .decode_ready(loop_ready),
-      .data_decode(loop_data),
-      .decode_read(loop_idle)
+      .decode_ready(decode_ready),
+      .data_decode(decode_data_out),
+      .decode_read(decode_ready)
   );
+
+  wire [31:0] encode_data_in;
+  wire rst_db;
+
+  debounce debounce_rst (
+      .clk(CLK),
+      .pb_in(RST),
+      .pb_out(rst_db)
+  );
+
+  reg rst_prev;
+  always @(posedge CLK) begin
+    rst_prev <= rst_db;
+  end
+  wire rst_edge = rst_db && !rst_prev;
 
   // --- Encode ---
   encode encode_inst (
       .clk(CLK),
-      .rst(RST),
-      .encode_en(loop_ready),
-      .data_decode(loop_data),
-      .encode_idle(loop_idle),
+      .rst(1'b1),
+      .encode_en(btn_edge),
+      .data_decode(encode_data_in),
+      .encode_idle(),
       .encode_ready(tx_en),
       .encode_read(tx_ready),
       .data_encode(tx_data)
   );
+
+  wire btn_db;
+  debounce debounce_btn (
+      .clk(CLK),
+      .pb_in(BTN),
+      .pb_out(btn_db)
+  );
+
+  reg btn_prev;
+  always @(posedge CLK) begin
+    btn_prev <= btn_db;
+  end
+  wire btn_edge = btn_db && !btn_prev;
+  wire state;
+  wire [31:0] reference_out;
+
+  // --- Reference ---
+  reference reference_inst (
+      .clk(CLK),
+      .rst(1'b1),
+      .reg_en(rst_edge),
+      .write_en(decode_ready),
+      .data_in(decode_data_out),
+      .state_out(state),
+      .data_out(reference_out)
+  );
+
+  // --- Controller ---
+  controller controller_inst (
+      .reference(reference_out),
+      .data_in  (decode_data_out),
+      .data_out (encode_data_in)
+  );
+
+  assign LED[1] = state;
 
 endmodule
