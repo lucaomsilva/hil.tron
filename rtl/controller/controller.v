af6d64eccf25ebb085acf44fffe0dc7786b6d851
Lucas Moura da Silva <lucas.silva16@unifesp.br>
2026-09-14 17:51:36 -0300

    refactor: use control and encode wire enable #36

--- rtl/controller/controller.v
+++ rtl/controller/controller.v
@@ -1,13 +1,31 @@
 module controller (
+    input wire clk,
+    input wire rst,
+
+    input wire control_en,
+
     input wire [31:0] reference,
     input wire [31:0] data_in,
 
-    output wire [31:0] data_out
+    output reg [31:0] data_out,
+    output reg encode_en
 );
 
   localparam Kp = 10;
 
   wire [31:0] error = reference - data_in;
-  assign data_out = Kp * error;
+
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      data_out <= 32'd0;
+      encode_en <= 1'b0;
+    end else begin
+      encode_en <= 1'b0;
+      if (control_en) begin
+        data_out <= Kp * error;
+        encode_en <= 1'b1;
+      end
+    end
+  end
 
 endmodule
